bd48f71ae9772979714d2c76c349a67c5ca3cfc3
JhonSun <1193086345.com>
2017-12-28 09:38:34 +0800

    Initial commit

---
+++ NSDictionary+Swizzle.h
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+Swizzle.h
+//  BPMS
+//
+//  Created by lianditech on 2017/12/28.
+//  Copyright © 2017年 啾三万. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (Swizzle)
+
+@end
